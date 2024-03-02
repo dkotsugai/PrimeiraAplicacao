1f94b436ca6a0e45f67f47e99ed5e43f3f86ce3a
Daniel Kotsugai <d.kotsugai.sarmento@avanade.com>
2024-03-02 20:20:17 -0300

    Convertendo valores

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Mavertick");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //comentario
+        /*
+
+        comentario
+        comentario
+
+        */
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double noraDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+        /*
+        DE / PARA :	byte	short	char	int	    long    float	 double
+        byte    	---     Impl.	(char)	Impl.	Impl.	 Impl.   Impl.
+        short	    (byte)	---     (char)	Impl.	Impl.	 Impl.	 Impl.
+        char	    (byte)	(char) 	---	    Impl.	Impl.	 Impl.	 Impl.
+        int	        (byte)	(char)	(char)   ---	Impl.	 Impl.	 Impl.
+        long	    (byte)	(char)	(char)	(int)	--- 	 Impl.	 Impl.
+        float	    (byte)	(char)	(char)	(int)	(long) 	  --- 	 Impl.
+        double	    (byte)	(char)	(char)	(int)	(long)	(float)	  ---
+         */
+
     }
 }
