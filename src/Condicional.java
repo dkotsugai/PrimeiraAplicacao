113d35e7db858983b1862a1ac48b00cbe6a159db
Daniel Kotsugai <d.kotsugai.sarmento@avanade.com>
2024-03-03 23:17:26 -0300

    Tomando decisões

---
+++ src/Condicional.java
@@ -0,0 +1,23 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean inclidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+
+        if (anoDeLancamento>2022){
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        }
+        else {
+            System.out.println("Filme retrô que vale a pena assistir");
+        }
+
+        // para comparativo de String sempre usar o parametro .equals
+        if(inclidoNoPlano == true || tipoPlano.equals("plus")){
+            System.out.println("Filme liberado");
+        }else {
+            System.out.println("Deve pagar a localção");
+        }
+    }
+}
